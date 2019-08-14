269ad14c193457b6633c3d63ae16bb4672d506fe
Kyle De Sousa <kyle@whitezip.com>
2019-08-14 16:22:20 +0100

    added new code

--- src/sizes/160x600/script.js
+++ src/sizes/160x600/script.js
@@ -35,6 +35,7 @@ import { TweenLite, TimelineMax} from 'gsap';
 var timeline = (function MasterTimeline() {
 	
 	var tl;
+	var tlBG;
 	var win = window;
 	
 	function doClickTag() { window.open(window.clickTag); }
@@ -46,6 +47,7 @@ var timeline = (function MasterTimeline() {
 	}
 	
 	function createTimeline() {
+		
 		tl = new TimelineMax({repeat: -1, onStart: updateStart, onComplete: updateComplete, onUpdate: updateStats});
 		
 		function nest(name){
@@ -62,15 +64,14 @@ var timeline = (function MasterTimeline() {
 		
 		var frameStart = 0.0;
 		
+		var bgCoins = nest('.coin-fall');
+		
 		var frlogo = nest('.fr-logo'),
 			fr01 = nest('.fr-f1'),
-			fr01Bg = nest('.fr-f1-bg'),
 			fr01Line1 = nest('.fr-f1-lines-l1'),
 			fr01Line2 = nest('.fr-f1-lines-l2'),
 			fr01Line3 = nest('.fr-f1-lines-l3'),
 			fr01Cta = nest('.fr-f1 .cta-btn'),
-			
-			
 			
 			fr02 = nest('.fr-f2'),
 			fr02Cube = nest('.fr-f2-slide-cube'),
@@ -89,31 +90,44 @@ var timeline = (function MasterTimeline() {
 			fr03Cta = nest('.fr-f3 .cta-btn'),
 			
 			fr04 = nest('.fr-f4'),
-			fr04Bg = nest('.fr-f4-bg'),
+			// fr04Bg = nest('.fr-f4-bg'),
 			fr04Line1 = nest('.fr-f4-lines-l1'),
 			fr04Line2 = nest('.fr-f4-lines-l2'),
 			fr04Line3 = nest('.fr-f4-lines-l3'),
 			fr04Cta = nest('.fr-f4 .cta-btn');
 
 		
+	
+		
 		function sceneOne(){
+			
+			
 			var tlBG = new TimelineMax({repeat: -1});
-			tlBG.to(fr01Bg, 25, {
+			tlBG.to(bgCoins, 25, {
 				backgroundPosition: "0 1200px",
 				ease: Power0.easeNone,
 				autoRound:false,
 			});
 			
+			var maxAnimationTime = 30;
+			
+			function stopAnimation(){
+				tlBG.pause();
+			};
+			
+			TweenLite.delayedCall(maxAnimationTime, stopAnimation);
+			
+			
 			var tl = new TimelineMax();
 			tl
 				.from(fr01,0.0, {opacity:1,force3D:true}, 0.1)
-				.from(fr01Bg, frSD, {opacity:1}, 0.2)
 				.from(frlogo, frSD, {opacity:1, ease:Elastic.easeOut.config(2, 1)}, 0.2)
 				.from(fr01Line1, frSD, {opacity:0, scale: 0.1, ease:Elastic.easeOut.config(2, 1)}, 0.2)
 				.from(fr01Line2, frSD, {opacity:0, scale: 0.1, ease:Elastic.easeOut.config(2, 1)}, 0.3)
 				.from(fr01Line3, frSD, {opacity:0, y: 20, ease:Elastic.easeOut.config(2, 1)}, 0.4)
 				.from(fr01Cta, frSD, {scale:0.1 ,opacity:0,  ease:Elastic.easeOut.config(1, 0.3)}, 0.4);
 			return tl;
+			
 		}
 		
 		function sceneTwo(){
@@ -121,7 +135,7 @@ var timeline = (function MasterTimeline() {
 			tl
 				.to(fr01,0.0, {opacity:0,force3D:true}, 0.1)
 				.from(fr02,0.0, {opacity:0,force3D:true}, 0.1)
-				.from(fr02Cube,frSD, {opacity:1, x:-555, transformOrigin:"50% 50%", ease:Power4.easeOut}, 0.3)
+				.from(fr02Cube,frSD, {opacity:1, x:-850, transformOrigin:"50% 50%", ease:Power4.easeOut}, 0.3)
 				.from(fr02Bg,frSD, {opacity:0}, 0.4)
 				.from(fr02Toon,frSD, {opacity:0,scale: 1.5, transformOrigin:"50% 50%", ease:Power4.easeOut}, 0.5)
 				.from(fr02Line1, frSD, {opacity:0, x: -50, ease:Elastic.easeOut.config(2, 1)}, 0.6)
@@ -148,30 +162,24 @@ var timeline = (function MasterTimeline() {
 		
 		function sceneFour(){
 			// Join over 1000 000 000
-			var tlBG = new TimelineMax({repeat: -1});
-			tlBG.to(fr04Bg, 25, {
-				backgroundPosition: "0 601px",
-				ease: Power0.easeNone,
-				autoRound:false,
-			});
-			
-			var Cont={val:0} , NewVal = 100 ;
 			
 			var tl = new TimelineMax();
 			tl
-				.to(fr03,0.0, {opacity:0,force3D:true}, 0.1)
+				.to(fr02Cube,frSD, {opacity:0, transformOrigin:"50% 50%", ease:Power4.easeOut}, 0.3)
+				.to(fr02Bg,frSD, {opacity:0}, 0.4)
+				.to(fr02Toon,frSD, {opacity:0,y: 200, scale: 0.8, transformOrigin:"50% 50%", ease:Power4.easeOut}, 0.5)
+				.to(fr03Line1, frSD, {opacity:0, scale:0.09, ease:Elastic.easeOut.config(2, 1)}, 0.4)
+				.to(fr03Line2, frSD, {opacity:0, scale:0.09, ease:Elastic.easeOut.config(2, 1)}, 0.5)
+				.to(fr03Line3, frSD, {opacity:0,scale:0.09, ease:Elastic.easeOut.config(2, 1)},0.6)
+				.to(fr03Providers, frSD, {opacity:0},1)
 				.from(fr04,1.0, {opacity:0,force3D:true}, 0.5)
-				.from(fr04Bg, frSD, {opacity:0}, 0.5)
 				.from(fr04Line1, frSD, {opacity:0, scale: 0.1, ease:Elastic.easeOut.config(2, 1)},  0.5)
-				.to(Cont,1,{val:1000000,roundProps:"val",onUpdate:function(){
-						fr04Line2.innerHTML=Cont.val
-					}})
 				.from(fr04Line2, frSD, {opacity:0, scale: 0.1, ease:Elastic.easeOut.config(2, 1)},  0.5)
 				.from(fr04Line3, frSD, {opacity:0, y: 20, ease:Elastic.easeOut.config(2, 1)},  0.5)
 				.from(fr04Cta, frSD, {scale:0.1 ,opacity:0 , ease:Elastic.easeOut.config(1, 0.3)},  0.5)
-				.to(fr04Line1, frSOD, {opacity:0}, 2.0)
-				.to(fr04Line2, frSOD, {opacity:0}, 2.0)
-				.to(fr04Line3, frSOD, {opacity:0}, 2.0);
+				.to(fr04Line1, frSOD, {opacity:0}, .1)
+				.to(fr04Line2, frSOD, {opacity:0}, .2)
+				.to(fr04Line3, frSOD, {opacity:0}, .3);
 			return tl;
 			
 			
@@ -179,14 +187,14 @@ var timeline = (function MasterTimeline() {
 		
 		tl
 			.add(sceneOne(), 'frame1+=0')
-			.add(sceneTwo(),'frame2+=1.5')
-			.add(sceneThree(), 'frame3+=1.5')
-			.add(sceneFour(), 'frame4+=1.5');
+			.add(sceneTwo(),'frame2+=0.0')
+			.add(sceneThree(), 'frame3+=0.0')
+			.add(sceneFour(), 'frame4+=0.0');
 		
 		// DEBUG:
 		// tl.play('frame2+=0'); // start playing at label:frame3
-		// tl.pause('frame1=1.0'); // pause the timeline at label:frame3
-		tl.play('frame2=1.0'); // pause the timeline at label:frame3
+		// tl.pause('frame4+=1.8'); // pause the timeline at label:frame3
+		// tl.play('frame1=1.0'); // pause the timeline at label:frame3
 	}
 	
 	function updateStart() {
@@ -230,7 +238,7 @@ timeline.init();
 	var maxAnimationTime = 30;
 	
 	function stopAnimation(){
-		tl.pause();
+		tl.pause('frame1+=0.9');
 	};
 	
 	TweenLite.delayedCall(maxAnimationTime, stopAnimation);
